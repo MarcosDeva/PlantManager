344f3617c6ea46088f1e246209377f9dbd8b5b4b
Marcos <marcos.oliveira.developer@gmail.com>
2023-08-18 13:25:44 -0300

    up

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 
-import { Welcome } from "./src/pages/Welcome";
 import * as SplashScreen from 'expo-splash-screen';
+import { 
+  useFonts, Jost_400Regular, Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+import { UserIdentification } from "./src/pages/UserIdentification";
 
 
 export default function App() {
-  
-  const [ fontsLoaded, fontError ] = useFonts({
-    Jost_400Regular, Jost_600SemiBold
+  // Correção não esta funcionando o carregamento das fontes
+  const [fontsLoaded, fontError] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
   });
 
+  
   if(!fontsLoaded && !fontError){
-    SplashScreen.preventAutoHideAsync();
+    // SplashScreen.preventAutoHideAsync();
   }
 
   return (
-    <Welcome/>
+    <UserIdentification/>
   );
 }
---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { SafeAreaView,View,Text,StyleSheet,TextInput,KeyboardAvoidingView,Platform} from "react-native";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+import { Button } from "../components/Button";
+
+export function UserIdentification() {
+
+  const [name, setName] = useState<String>('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  
+  function handleInputFocus() {
+    setIsFocused(false);
+    setIsFocused(!!name);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: String) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.content}>
+          
+          <View style={styles.form}>
+            <View style={styles.header}>
+                <Text style={styles.emoji}>
+                  { isFilled ? '😆' : '😀' }
+                </Text>
+
+                <Text style={styles.title}>
+                Como podemos {"\n"}
+                chamar você
+                </Text>
+            </View>
+            <TextInput 
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && 
+                  { borderColor: colors.green }
+                ]} 
+                placeholder="Digite seu nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus} 
+                onChangeText={handleInputChange}
+            />
+
+            <View style={styles.footer}>
+              <Button />
+            </View>
+
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+
+  content: {
+    flex: 1,
+    width: "100%",
+  },
+
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 54,
+    alignItems: "center",
+  },
+
+  header: {
+    alignItems:'center'
+  },
+
+  emoji: {
+    fontSize: 44,
+  },
+
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: "100%",
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: "center",
+  },
+
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: "center",
+    color: colors.heading,
+    // fontFamily: fonts.heading,
+    marginTop: 20,
+  },
+
+  footer: {
+    width: "100%",
+    marginTop: 40,
+    paddingVertical: 20,
+  },
+
+});
